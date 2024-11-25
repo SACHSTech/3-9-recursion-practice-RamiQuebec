e4307d17b4f5e82b212ea9461a529cd8069f6148
RamiQuebec <rami.kabak25@ycdsbk12.ca>
2024-11-25 10:40:32 -0500

    Sum of Natural numbers

--- src/recursion/SumofNaturalNumbers.java
+++ src/recursion/SumofNaturalNumbers.java
@@ -2,10 +2,14 @@ package recursion;
 
 public class SumofNaturalNumbers {
     public static int sum(int n) {
-        return 0;
+        if (n == 0) {
+            return 0;
+        } else {
+            return n + sum(n - 1);
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(5)); // Output: 15
+        System.out.println(sum(5));
     }
 }
